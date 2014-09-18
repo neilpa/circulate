71e4864aa9f051bac51a59337699e540d2a05094
Neil Pankey <neil@wire.im>
2014-09-17 22:11:38 -0700

    Add library project template

---
+++ libCirculate/libCirculate.h
@@ -0,0 +1,13 @@
+//
+//  libCirculate.h
+//  libCirculate
+//
+//  Created by Neil Pankey on 9/17/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libCirculate : NSObject
+
+@end
---
+++ libCirculate/libCirculate.m
@@ -0,0 +1,13 @@
+//
+//  libCirculate.m
+//  libCirculate
+//
+//  Created by Neil Pankey on 9/17/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+#import "libCirculate.h"
+
+@implementation libCirculate
+
+@end
