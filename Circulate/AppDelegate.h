6e3abe38e4be0634a2ea0cc68206651598028825
Neil Pankey <neil@wire.im>
2014-09-17 22:00:11 -0700

    Initial commit

---
+++ Circulate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Circulate
+//
+//  Created by Neil Pankey on 9/17/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Circulate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Circulate
+//
+//  Created by Neil Pankey on 9/17/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
