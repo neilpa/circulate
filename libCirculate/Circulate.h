e3cfd315c47b1cc821d3c3803854ea1986d2544f
Neil Pankey <neil@wire.im>
2014-09-17 23:02:12 -0700

    Project structure

--- libCirculate/Circulate.h
+++ libCirculate/Circulate.h
@@ -1,13 +1,9 @@
 //
-//  libCirculate.m
-//  libCirculate
+//  Circulate.h
+//  Circulate
 //
 //  Created by Neil Pankey on 9/17/14.
 //  Copyright (c) 2014 Neil Pankey. All rights reserved.
 //
 
-#import "libCirculate.h"
-
-@implementation libCirculate
-
-@end
+// TODO Import library header files
