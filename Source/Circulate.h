98024aa77b3205b2da21a0962193cc3da18e9f94
Neil Pankey <npankey@gmail.com>
2015-05-26 14:02:10 -0700

    Stub out Swift framework

---
+++ Source/Circulate.h
@@ -0,0 +1,19 @@
+//
+//  Circulate.h
+//  Circulate
+//
+//  Created by Neil Pankey on 5/26/15.
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Circulate.
+FOUNDATION_EXPORT double CirculateVersionNumber;
+
+//! Project version string for Circulate.
+FOUNDATION_EXPORT const unsigned char CirculateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Circulate/PublicHeader.h>
+
+
