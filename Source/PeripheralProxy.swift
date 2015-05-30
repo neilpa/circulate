ccbedaa606a7ff469a9bb8dbb76b8aab40472cfe
Neil Pankey <npankey@gmail.com>
2015-05-30 13:26:01 -0700

    Delete copy/paste comments

--- Source/PeripheralProxy.swift
+++ Source/PeripheralProxy.swift
@@ -68,110 +68,35 @@ internal final class PeripheralProxy: NSObject, CBPeripheralDelegate {
         sendNext(_nameSink, peripheral.name ?? "")
     }
 
-    /*!
-    *  @method peripheral:didDiscoverServices:
-    *
-    *  @param peripheral	The peripheral providing this information.
-    *	@param error		If an error occurred, the cause of the failure.
-    *
-    *  @discussion			This method returns the result of a @link discoverServices: @/link call. If the service(s) were read successfully, they can be retrieved via
-    *						<i>peripheral</i>'s @link services @/link property.
-    *
-    */
     func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
         println("\(peripheral) \(error)")
         sendNext(_serivceSink, peripheral.services.map { $0 as! CBService })
     }
 
-    /*!
-    *  @method peripheral:didDiscoverCharacteristicsForService:error:
-    *
-    *  @param peripheral	The peripheral providing this information.
-    *  @param service		The <code>CBService</code> object containing the characteristic(s).
-    *	@param error		If an error occurred, the cause of the failure.
-    *
-    *  @discussion			This method returns the result of a @link discoverCharacteristics:forService: @/link call. If the characteristic(s) were read successfully,
-    *						they can be retrieved via <i>service</i>'s <code>characteristics</code> property.
-    */
     func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
         sendNext(_characteristicSink, service)
     }
 
-    /*!
-    *  @method peripheral:didUpdateValueForCharacteristic:error:
-    *
-    *  @param peripheral		The peripheral providing this information.
-    *  @param characteristic	A <code>CBCharacteristic</code> object.
-    *	@param error			If an error occurred, the cause of the failure.
-    *
-    *  @discussion				This method is invoked after a @link readValueForCharacteristic: @/link call, or upon receipt of a notification/indication.
-    */
     func peripheral(peripheral: CBPeripheral!, didUpdateValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
         sendNext(_readSink, characteristic)
     }
 
-    /*!
-    *  @method peripheral:didWriteValueForCharacteristic:error:
-    *
-    *  @param peripheral		The peripheral providing this information.
-    *  @param characteristic	A <code>CBCharacteristic</code> object.
-    *	@param error			If an error occurred, the cause of the failure.
-    *
-    *  @discussion				This method returns the result of a {@link writeValue:forCharacteristic:type:} call, when the <code>CBCharacteristicWriteWithResponse</code> type is used.
-    */
     func peripheral(peripheral: CBPeripheral!, didWriteValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
         sendNext(_writeSink, characteristic)
     }
 
-    /*!
-    *  @method peripheral:didUpdateNotificationStateForCharacteristic:error:
-    *
-    *  @param peripheral		The peripheral providing this information.
-    *  @param characteristic	A <code>CBCharacteristic</code> object.
-    *	@param error			If an error occurred, the cause of the failure.
-    *
-    *  @discussion				This method returns the result of a @link setNotifyValue:forCharacteristic: @/link call.
-    */
     func peripheral(peripheral: CBPeripheral!, didUpdateNotificationStateForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
         sendNext(_updateSink, characteristic)
     }
 
-    /*!
-    *  @method peripheral:didDiscoverDescriptorsForCharacteristic:error:
-    *
-    *  @param peripheral		The peripheral providing this information.
-    *  @param characteristic	A <code>CBCharacteristic</code> object.
-    *	@param error			If an error occurred, the cause of the failure.
-    *
-    *  @discussion				This method returns the result of a @link discoverDescriptorsForCharacteristic: @/link call. If the descriptors were read successfully,
-    *							they can be retrieved via <i>characteristic</i>'s <code>descriptors</code> property.
-    */
     func peripheral(peripheral: CBPeripheral!, didDiscoverDescriptorsForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
 
     }
 
-    /*!
-    *  @method peripheral:didUpdateValueForDescriptor:error:
-    *
-    *  @param peripheral		The peripheral providing this information.
-    *  @param descriptor		A <code>CBDescriptor</code> object.
-    *	@param error			If an error occurred, the cause of the failure.
-    *
-    *  @discussion				This method returns the result of a @link readValueForDescriptor: @/link call.
-    */
     func peripheral(peripheral: CBPeripheral!, didUpdateValueForDescriptor descriptor: CBDescriptor!, error: NSError!) {
 
     }
 
-    /*!
-    *  @method peripheral:didWriteValueForDescriptor:error:
-    *
-    *  @param peripheral		The peripheral providing this information.
-    *  @param descriptor		A <code>CBDescriptor</code> object.
-    *	@param error			If an error occurred, the cause of the failure.
-    *
-    *  @discussion				This method returns the result of a @link writeValue:forDescriptor: @/link call.
-    */
     func peripheral(peripheral: CBPeripheral!, didWriteValueForDescriptor descriptor: CBDescriptor!, error: NSError!) {
         
     }
